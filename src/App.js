d1c5602ea5dcb99d65fedcbf00e00442a9def499
naraeseo <naraeseoez@gmail.com>
2022-09-21 17:51:23 +0900

    Section 11 143: Meals의 컴포넌트들 추가 후 컴포넌트들 연결

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from 'react';
 import Header from './Component/Layout/Header';
+import Meals from './Component/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
