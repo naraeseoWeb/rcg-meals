c76a0d8cbf5b6e5fe097a689d9af1034f324312e
naraeseo <naraeseoez@gmail.com>
2022-09-22 14:20:46 +0900

    Section 11 148: modal 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
+import Cart from './Component/Cart/Cart';
 import Header from './Component/Layout/Header';
 import Meals from './Component/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
