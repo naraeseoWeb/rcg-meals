2f422eb201c667672b57f773a3cdd77ef24a8f8a
naraeseo <naraeseoez@gmail.com>
2022-09-22 14:44:41 +0900

    Section 11 149: modal 컴포넌트에서 열고 닫힘 제어하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import Cart from './Component/Cart/Cart';
 import Header from './Component/Layout/Header';
 import Meals from './Component/Meals/Meals';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
